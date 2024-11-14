f656ce900425082a5026773b9f74145cd5b008e5
Thasu11 <zkpatel0912@gmail.com>
2024-11-14 20:21:30 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+
+def triangle():
+  
+
+
+#set points
+
+triangle((50, 150), (200, 50), (300, 250))
+
+
+while True:
+    screen.fill((255, 255, 255))
+    
+    pygame.display.flip()
